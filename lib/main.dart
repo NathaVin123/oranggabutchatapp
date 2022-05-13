fa2ab82125c00e853a97a53d31de974bd327ca27
Nathaniel Vincent Wibowo <vincentnathaniel4@gmail.com>
2022-05-14 00:11:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BaseApp());
+}
+
+class BaseApp extends StatelessWidget {
+  const BaseApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OrangGabut Chat App',
+      theme: ThemeData(),
+      home: const BasePage(),
+    );
+  }
+}
+
+class BasePage extends StatefulWidget {
+  const BasePage({Key? key}) : super(key: key);
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OrangGabut'),
+      ),
+      body: Container(),
+    );
+  }
+}
